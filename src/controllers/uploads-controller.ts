f8f6900685579d71f0543bdbb8742201acc68bd9
gazimba <gabrielb3r@gmail.com>
2025-03-31 16:50:55 -0300

    a

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 class UploadsController {
     async create(request: Request, response: Response) {
         response.json({
-            message: "uhul"
+            file: request.file
         })
     }
 }
